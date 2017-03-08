a628c3235f39c87e325ac3d7495cec72297ec4da
Emmanuel Baccelli <emmanuel.baccelli@inria.fr>
2017-03-08 12:23:29 +0100

    removing commented-out code

--- examples/javascript/main.c
+++ examples/javascript/main.c
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2017 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Showing an example of scripting (javascript) from command line
+ *
+ * @author      Emmanuel Baccelli <emmanuel.baccelli@inria.fr>
+ *
+ * @}
+ */
+
 #include <string.h>
 #include "shell.h"
 #include "jerryscript.h"
